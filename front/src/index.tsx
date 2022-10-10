b483ba13e29cfb7157f04a9c8d00288a30febc12
KimKW1007 <face676934@gmail.com>
2022-10-10 14:27:59 +0900

    feat : 글로벌css 추가

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GlobalStyle } from './styles/resetStyled';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,6 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <RecoilRoot>
+      <GlobalStyle/>
       <App />
     </RecoilRoot>
   </React.StrictMode>
