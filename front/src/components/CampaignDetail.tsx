305d6bcb4dbb0b6269b3b577136445a6e5c56408
KimKW1007 <face676934@gmail.com>
2022-10-10 18:11:56 +0900

    feat : 댓글창 추가

--- front/src/components/CampaignDetail.tsx
+++ front/src/components/CampaignDetail.tsx
@@ -5,7 +5,7 @@ const BtnBox = styled.div`
   display:flex;
   justify-content: flex-end;
   button{
-    margin-left:20px;
+    margin-left:10px;
   }
 `
 
@@ -16,6 +16,54 @@ const JoinCampaignBox = styled.div`
     height:70px;
   }
 
+`
+
+const CommentContainer = styled.div`
+  padding : 30px 70px;
+  border : 1px solid #000;
+  margin: 0 auto;
+  .writeCommentBox{
+    display:flex;
+    padding:  25px 0;
+    height:80px;
+    border-bottom : 1px solid #000;
+    
+    input{
+      border :none;
+      width:100%;
+      height:100%;
+      &:focus{
+        outline:none;
+      }
+    }
+  }
+  .userImgBox{
+    margin: 0 30px;
+    height:30px;
+    width:30px;
+    flex-shrink:0;
+    border : 1px solid #000;
+    img{
+
+    }
+  }
+`
+
+const CommentListBox = styled.div`
+display:flex;
+padding:  25px 0;
+.userImgBox{
+  margin-top: 6px;
+}
+.commentBox{
+
+    .userName{
+      margin-bottom:10px;
+    }
+    .commnet{
+
+    }
+}
 `
 
 export default function CampaignDetail() {
@@ -32,7 +80,34 @@ export default function CampaignDetail() {
         </button>
       </JoinCampaignBox>
       {/*  */}
-
+      <CommentContainer>
+        <div className="writeCommentBox">
+          <div className="userImgBox">
+            <img src="" alt="" />
+          </div>
+          <input type="text" placeholder="댓글 달기..." />
+        </div>
+        <CommentListBox>
+            <div className="userImgBox">
+              <img src="" alt="" />
+            </div>
+            <div className="commentBox">
+              <div className="userName">
+                금잔디 명예소방관
+              </div>
+              <div className="commnet">
+                {/* {xx.length >= 100 ? 
+                  <>
+                  xx.slice(0,50)+"..." 
+                  <button onClick={()=>{isTrue(!true)}}>
+                    {isTrue ? 간략히 보기 : 자세히 보기 }
+                  </button>
+                  </>:
+                   xx} */}
+              </div>
+            </div>
+        </CommentListBox>
+      </CommentContainer>
     </div>
   )
 }
