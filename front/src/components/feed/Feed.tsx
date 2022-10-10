5fc4a2ba32b2e37157319246e010b818b0efd4c2
DaHyunOhe <peelsob4d@gmail.com>
2022-10-10 19:18:21 +0900

    style: 피드 기능 메뉴, 슬라이더 스타일 임시 추가

--- front/src/components/feed/Feed.tsx
+++ front/src/components/feed/Feed.tsx
@@ -31,12 +31,20 @@ function Feed({ images, isClass, author }: feedProps) {
             />
           ))}
         </ImageList>
-        <ButtonContainer>
-          <button onClick={() => handleViewerClick(false)}>Prev</button>
-          <button onClick={() => handleViewerClick(true)}>Next</button>
-        </ButtonContainer>
+        <AlbumSlideContainer>
+          <AlbumSlideButton onClick={() => handleViewerClick(false)}>
+            {'<'}
+          </AlbumSlideButton>
+          <AlbumSlideButton onClick={() => handleViewerClick(true)}>
+            {'>'}
+          </AlbumSlideButton>
+        </AlbumSlideContainer>
       </Album>
       <MenuContainer>
+        <Likes>
+          <LikesIcon />
+          <LikesCount>{100}</LikesCount>
+        </Likes>
         {IMAGE_LAST_INDEX !== IMAGE_FIRST_INDEX && (
           <AlbumNavigator>
             {images!.map((value, index) => (
@@ -44,6 +52,10 @@ function Feed({ images, isClass, author }: feedProps) {
             ))}
           </AlbumNavigator>
         )}
+        <SocialAlbumSlideContainer>
+          <SocialButton />
+          <SocialButton />
+        </SocialAlbumSlideContainer>
       </MenuContainer>
       <ProfileContainer></ProfileContainer>
     </FeedContainer>
@@ -69,14 +81,25 @@ const ImageList = styled.div<{ albumPage: number }>`
   transform: ${(props) => `translateX(-${props.albumPage * 100}%)`};
 `;
 
-const ButtonContainer = styled.div`
-  width: 100%;
+const AlbumSlideContainer = styled.div`
+  width: calc(100% - 26px);
   height: 100%;
   position: absolute;
   top: 0;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  padding: 0 13px 0 13px;
+`;
+
+const AlbumSlideButton = styled.button`
+  width: 24px;
+  height: 24px;
+  background-color: rgba(52, 52, 52, 0.15);
+  color: rgba(255, 255, 255, 0.8);
+  border: 0px;
+  border-radius: 24px;
+  font-size: 16px;
 `;
 
 const MenuContainer = styled.div`
@@ -84,12 +107,16 @@ const MenuContainer = styled.div`
   height: 50px;
   border-bottom: 1px solid #e7e1d2;
   display: flex;
+  align-items: center;
 `;
 
 const AlbumNavigator = styled.div`
   flex: 1;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
+  & > div {
+    margin-right: 2px;
+  }
 `;
 
 const Navigator = styled.div<{
@@ -103,6 +130,48 @@ const Navigator = styled.div<{
     props.observeIndex === props.currentIndex
       ? '#E0D4B7'
       : 'rgba(231, 225, 210, 0.5)'};
+  &:last-child {
+    margin-right: 0px;
+  }
+`;
+
+const Likes = styled.div`
+  margin-left: 21px;
+  height: 22px;
+  display: flex;
+  align-items: center;
+`;
+
+const LikesIcon = styled.img`
+  width: 22px;
+  height: 22px;
+  object-fit: contain;
+`;
+
+const LikesCount = styled.div`
+  margin-left: 8px;
+  color: #000000;
+  height: 19px;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 17px;
+`;
+
+const SocialAlbumSlideContainer = styled.div`
+  margin-right: 17px;
+  height: 22px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const SocialButton = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 20px;
+  border: 2px solid black;
+  &:first-child {
+    margin-right: 12px;
+  }
 `;
 
 const ProfileContainer = styled.div`
