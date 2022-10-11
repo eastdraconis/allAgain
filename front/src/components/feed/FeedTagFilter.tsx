9a550098b996cc7a9d05e74a4bf77faa47407e67
DaHyunOhe <peelsob4d@gmail.com>
2022-10-11 17:25:41 +0900

    feat: 버튼 생성 로직 변경, 스타일 수정

--- front/src/components/feed/FeedTagFilter.tsx
+++ front/src/components/feed/FeedTagFilter.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled, { css } from 'styled-components';
+import { SaveButton } from '../common/Buttons';
 
 type TagTypes =
   | 'all'
@@ -12,77 +13,42 @@ type TagTypes =
   | 'rubber'
   | 'etc';
 
+const TagList = {
+  all: '전체',
+  plastic: '플라스틱',
+  fiber: '섬유',
+  wood: '나무',
+  paper: '종이류',
+  glass: '유리',
+  metal: '금속',
+  rubber: '고무',
+  etc: '그 외',
+};
+
 function FeedTagFilter() {
   const [selectedTag, setSelectedTag] = useState<TagTypes>('all');
 
   const handleTagButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setSelectedTag((event.target as HTMLButtonElement).value as TagTypes);
-    console.log((event.target as HTMLButtonElement).value);
   };
 
   return (
     <TagButtonContainer>
-      <TagButton
-        value='all'
-        selectedTag={selectedTag}
-        onClick={handleTagButtonClick}>
-        전체
-      </TagButton>
-      <TagButton
-        value='plastic'
-        selectedTag={selectedTag}
-        onClick={handleTagButtonClick}>
-        플라스틱
-      </TagButton>
-      <TagButton
-        value='fiber'
-        selectedTag={selectedTag}
-        onClick={handleTagButtonClick}>
-        섬유
-      </TagButton>
-      <TagButton
-        value='wood'
-        selectedTag={selectedTag}
-        onClick={handleTagButtonClick}>
-        나무
-      </TagButton>
-      <TagButton
-        value='paper'
-        selectedTag={selectedTag}
-        onClick={handleTagButtonClick}>
-        종이류
-      </TagButton>
-      <TagButton
-        value='glass'
-        selectedTag={selectedTag}
-        onClick={handleTagButtonClick}>
-        유리
-      </TagButton>
-      <TagButton
-        value='metal'
-        selectedTag={selectedTag}
-        onClick={handleTagButtonClick}>
-        금속
-      </TagButton>
-      <TagButton
-        value='rubber'
-        selectedTag={selectedTag}
-        onClick={handleTagButtonClick}>
-        고무
-      </TagButton>
-      <TagButton
-        value='etc'
-        selectedTag={selectedTag}
-        onClick={handleTagButtonClick}>
-        그 외
-      </TagButton>
+      {Object.entries(TagList).map(([key, value]) => (
+        <TagButton
+          value={key}
+          selectedTag={selectedTag}
+          onClick={handleTagButtonClick}>
+          {value}
+        </TagButton>
+      ))}
     </TagButtonContainer>
   );
 }
 
 const TagButtonContainer = styled.div`
   margin-top: 174px;
-  width: 748px;
+  width: 810px;
   height: 71px;
   background-color: #f2eee3;
   border-radius: 5px;
@@ -91,25 +57,26 @@ const TagButtonContainer = styled.div`
   padding: 0 30px;
 `;
 
-const TagButton = styled.button<{ value: string; selectedTag: TagTypes }>`
+const TagButton = styled(SaveButton)<{ value: string; selectedTag: TagTypes }>`
+  width: auto;
   height: 35px;
   font-size: 16px;
-  font-weight: 400;
-  line-height: 19px;
-  text-align: center;
-  border: 0;
+  padding: 0 18px;
   border-radius: 5px;
-  padding: 1px 18px;
+  box-shadow: none;
   ${(props) =>
     props.value === props.selectedTag
       ? css`
           background-color: #afa58d;
-          color: #000000;
+          color: #ffffff;
         `
       : css`
           background-color: rgba(0, 0, 0, 0);
           color: #928b7a;
         `}
+  &:hover {
+    color: #ffffff;
+  }
 `;
 
 export default FeedTagFilter;
