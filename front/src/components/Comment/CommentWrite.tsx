2106cd915b5cfd79119018df3bfab6aff8a9fee7
KimKW1007 <face676934@gmail.com>
2022-10-12 13:02:54 +0900

    chore :  css 및 구조변경

--- front/src/components/Comment/CommentWrite.tsx
+++ front/src/components/Comment/CommentWrite.tsx
@@ -1,25 +1,24 @@
-import styled from 'styled-components'
-import { useForm, SubmitHandler } from "react-hook-form";
+import styled from 'styled-components';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import sendHoverIcon from '../../assets/images/icons/icon_send_hover.png';
 import sendIcon from '../../assets/images/icons/icon_send.png';
-import UserImgBox from '../CampaignItems/UserImgBox';
-
+import UserImgBox from './UserImgBox';
 
 const CommentWriteBox = styled.div`
   display: flex;
   padding: 15px 0 15px;
-  align-items:center;
+  align-items: center;
   height: 70px;
   border-bottom: 1px solid rgba(231, 225, 210, 1);
-  &.ReComment{
+  &.ReComment {
     margin-left: 50px;
   }
-`
+`;
 
 const CommentFrom = styled.form`
-  width:100%;
-  display:flex;
-  align-items:center;
+  width: 100%;
+  display: flex;
+  align-items: center;
   input {
     background: inherit;
     border: none;
@@ -29,41 +28,45 @@ const CommentFrom = styled.form`
       outline: none;
     }
   }
-`
-
+`;
 
 const SubmitIconBtn = styled.button`
-  width:20px;
-  height:20px;
+  width: 20px;
+  height: 20px;
   background: url(${sendIcon}) center center no-repeat;
-  background-size:cover;
-  margin-left:20px;
-  transition: background .3s;
-  &:hover{
+  background-size: cover;
+  margin-left: 20px;
+  transition: background 0.3s;
+  &:hover {
     background-image: url(${sendHoverIcon});
   }
-`
+`;
 
 interface ReCommentType {
-  isReCommentWrite ?:Boolean;
+  isReCommentWrite?: Boolean;
 }
 
 interface WriteInputType {
-  commentWrite ?: string;
+  commentWrite?: string;
 }
 
-export default function CampaignCommentWrite({isReCommentWrite}: ReCommentType) {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm<WriteInputType>();
-  const onSubmit: SubmitHandler<WriteInputType> = ({commentWrite}) => {
-      console.log("commentWrite",commentWrite);
-  }
+export default function CampaignCommentWrite({ isReCommentWrite }: ReCommentType) {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors }
+  } = useForm<WriteInputType>();
+  const onSubmit: SubmitHandler<WriteInputType> = ({ commentWrite }) => {
+    console.log('commentWrite', commentWrite);
+  };
   return (
-    <CommentWriteBox className={isReCommentWrite ? "ReComment" : ""} >
-      <UserImgBox/>
+    <CommentWriteBox className={isReCommentWrite ? 'ReComment' : ''}>
+      <UserImgBox />
       <CommentFrom onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" placeholder="댓글 달기..." {...register("commentWrite")} />
+        <input type="text" placeholder="댓글 달기..." {...register('commentWrite')} />
         <SubmitIconBtn type="submit"></SubmitIconBtn>
       </CommentFrom>
     </CommentWriteBox>
-  )
+  );
 }
