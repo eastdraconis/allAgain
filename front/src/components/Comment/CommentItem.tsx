617754c1fd4310e7667ce01b7292ea74f89b544d
KimKW1007 <face676934@gmail.com>
2022-10-12 12:59:03 +0900

    chore :  css 변경 및 자세히보기 테스트

--- front/src/components/Comment/CommentItem.tsx
+++ front/src/components/Comment/CommentItem.tsx
@@ -2,47 +2,56 @@ import styled from 'styled-components';
 import { useState } from 'react';
 import CampaignCommentWrite from './CommentWrite';
 import CampaignUtilsBox from './CommentUtilsBox';
-import UserImgBox from '../CampaignItems/UserImgBox';
+import UserImgBox from './UserImgBox';
+import UserName from '../common/UserName';
 
 const CommentBox = styled.div`
   display: flex;
   padding: 25px 0 0;
   > .userImgBox {
-    margin: 10px 15px 0 30px  !important;
-    
+    margin: 10px 15px 0 30px !important;
   }
   .commentTextBox {
-    .userName {
-      display:inline-block;
+    .userNameBox{
       margin-bottom: 5px;
+    }
+    .userName {
       font-weight: bold;
       font-size: 15px;
     }
     .comment {
       font-size: 14px;
+
+      p{
+
+      }
+      button{
+        font-size: 13px;
+        color: ${({theme})=> theme.colors.darkBeige};
+      }
     }
   }
 `;
 
 export default function CommentItem() {
   const [isReComment, setIsReComment] = useState(false);
-
+  const [ts, setTs] = useState(`시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관시켜줘 너의 명예소방관`);
+  const [isLong, setIsLong] = useState(false); 
   return (
     <>
       <CommentBox>
-        <UserImgBox/>
+        <UserImgBox />
         <div className="commentTextBox">
-          <div className="userName">금잔디 명예소방관</div>
+          <UserName/>
           <div className="comment">
-            {/* {xx.length >= 100 ? 
+            {ts.length >= 100 ? 
                 <>
-                xx.slice(0,50)+"..." 
-                <button onClick={()=>{isTrue(!true)}}>
-                  {isTrue ? 간략히 보기 : 자세히 보기 }
+                <p>{isLong ? ts : ts.slice(0,50)+"..." }<br/></p>
+                <button onClick={()=>{setIsLong(!isLong)}}>
+                  {isLong ? <>간략히 보기</> : <>자세히 보기</> }
                 </button>
                 </>:
-                xx} */}
-            시켜줘 너의 명예소방관
+                ts}
           </div>
           <CampaignUtilsBox setIsReComment={setIsReComment} />
         </div>
