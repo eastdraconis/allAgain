47d3383c4b00c8eb7ee471b3ffd86392ec77474d
[AI5/1반] 정소희 <jshee940315@gmail.com>
2022-10-12 11:34:13 +0900

    fix: 공통 컴포넌트 잘못된 스타일 선언위치 수정, 구글폰트 import위치 수정

--- front/src/components/Header/Header.tsx
+++ front/src/components/Header/Header.tsx
@@ -3,43 +3,43 @@ import { Link } from "react-router-dom";
 import { Container } from "../common/Containers";
 import NavBar from "../Nav/NavBar";
 
-export default function Header() {
+const HeaderWrap = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 70px;
+  z-index: 1000;
 
-  const HeaderWrap = styled.header`
-    position: fixed;
-    top: 0;
-    left: 0;
+  ${Container} {
+    position: relative;
+    height: 100%;
+  }
+`;
+
+const Logo = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 80px;
+  transform: translate(0, -50%);
+  width: 200px;
+  height: 70px;
+  background: #E0D4B7;
+  cusor: pointer;
+  z-index: 10;
+
+  a {
+    display: block;
     width: 100%;
-    height: 70px;
-    z-index: 1000;
+    height: 100%;
+  }
+`;
 
-    ${Container} {
-      position: relative;
-      height: 100%;
-    }
-  `;
+const HeaderBtns = styled.div`
 
-  const Logo = styled.div`
-    position: absolute;
-    top: 50%;
-    left: 80px;
-    transform: translate(0, -50%);
-    width: 200px;
-    height: 70px;
-    background: #E0D4B7;
-    cusor: pointer;
-    z-index: 10;
+`;
 
-    a {
-      display: block;
-      width: 100%;
-      height: 100%;
-    }
-  `;
-
-  const HeaderBtns = styled.div`
-  
-  `;
+export default function Header() {
 
   return (
     <HeaderWrap>
--- front/src/components/Nav/NavBar.tsx
+++ front/src/components/Nav/NavBar.tsx
@@ -1,37 +1,37 @@
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
+let activeStyle = {
+  textDecoration: "underline",
+};
+
+let activeClassName = "underline";
+
+const NavListWrap = styled.nav`
+  width: 100%;
+  height: 100%;
+`;
+
+const NavList = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`
+
+const NavListLi = styled.li`
+  padding: 10px;
+  margin: 0 60px;
+
+  a {
+    color: ${({ theme }) => theme.colors.brown};
+    font-size: 17px;
+    font-weight: 500;
+  }
+`
 
 export default function NavBar() {
-  let activeStyle = {
-    textDecoration: "underline",
-  };
-
-  let activeClassName = "underline";
-
-  const NavListWrap = styled.nav`
-    width: 100%;
-    height: 100%;
-  `;
-
-  const NavList = styled.ul`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-  `
-
-  const NavListLi = styled.li`
-    padding: 10px;
-    margin: 0 60px;
-
-    a {
-      color: ${({ theme }) => theme.colors.brown};
-      font-size: 17px;
-      font-weight: 500;
-    }
-  `
-
+  
   return (
     <NavListWrap>
       <NavList>
