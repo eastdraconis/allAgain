d148c8fd1c37cf859b88bfde333646535137e3be
KimKW1007 <face676934@gmail.com>
2022-10-12 10:07:48 +0900

    feat : 태그 버튼 추가

---
+++ front/src/components/CampaignItems/TagBox.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const TagListBox = styled.div`
+  width: 650px;
+  height: 70px;
+  background: #f2eee3;
+  margin-bottom: 50px;
+  display: inline-flex;
+  align-items: center;
+  padding: 0 30px;
+  border-radius: 5px;
+`;
+const TagItemBox = styled.button`
+  padding: 10px 12px;
+  margin: 0 10px;
+  border-radius: 5px;
+  background: inherit;
+  color: ${({ theme }) => theme.colors.lightBeige};
+  &.active {
+    background: ${({ theme }) => theme.colors.darkBeige};
+    color: ${({ theme }) => theme.colors.white};
+  }
+`;
+
+interface PropsType {
+  values : String[];
+  currentValue : String;
+  setCurrentValue : React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function TagBox({values, currentValue, setCurrentValue}: PropsType) {
+  
+  const handleChangeCurrentState = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const event = e.target as HTMLButtonElement;
+    const { value } = event;
+    if (value !== currentValue) {
+      setCurrentValue(value);
+    }
+  };
+  return (
+    <TagListBox>
+      {values.map((element) => (
+        <TagItemBox 
+          key={`${element}`}
+          className={currentValue === element ? 'active' : ''}
+          value={`${element}`} 
+          onClick={handleChangeCurrentState}>
+          {element}
+        </TagItemBox>
+      ))}
+    </TagListBox>
+  );
+}
