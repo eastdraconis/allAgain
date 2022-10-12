d903699f9acbe4b13dd6426511def0724c607d27
KimKW1007 <face676934@gmail.com>
2022-10-12 10:10:11 +0900

    chore : 유저이미지 변경으로 수정됨

--- front/src/components/Comment/Comments.tsx
+++ front/src/components/Comment/Comments.tsx
@@ -2,21 +2,14 @@ import styled from 'styled-components';
 import { useState } from 'react';
 import CampaignUtilsBox from './CommentUtilsBox';
 import CampaignCommentWrite from './CommentWrite';
-import CampaignCommentBox from './CommentItem';
+import CampaignCommentItem from './CommentItem';
 
 const CommentContainer = styled.div`
   padding: 30px 70px;
   border: 1px solid rgba(231, 225, 210, 0.8);
-  margin: 0 0 500px;
   box-shadow: ${({ theme }) => theme.boxShadowDefault};
   background: ${({ theme }) => theme.colors.white};
-  .userImgBox {
-    margin: 0 30px;
-    height: 30px;
-    width: 30px;
-    flex-shrink: 0;
-    border: 1px solid #000;
-  }
+
 `;
 
 const CommentListBox = styled.div``;
@@ -30,7 +23,8 @@ export default function Comments() {
     <CommentContainer>
       <CampaignCommentWrite />
       <CommentListBox>
-        <CampaignCommentBox />
+        <CampaignCommentItem />
+        <CampaignCommentItem />
       </CommentListBox>
     </CommentContainer>
   );
