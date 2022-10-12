d4712fd68955f8775bea08ad3cc5bc15c757b1f0
KimKW1007 <face676934@gmail.com>
2022-10-12 18:00:06 +0900

    test : 탭마다 데이터 필터해서 리스트화 테스트 중

--- front/src/pages/CampaignPage/CampaignPage.tsx
+++ front/src/pages/CampaignPage/CampaignPage.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import CampaignDetail from '../../components/CampaignItems/CampaignDetail';
 import CampaignItem from '../../components/CampaignItems/CampaignItem';
 import TagBox from '../../components/CampaignItems/TagBox';
 import { ConfirmButton } from '../../components/common/Buttons';
 import { Container, Container1300 } from '../../components/common/Containers';
-
+import { useRecoilValue } from "recoil"
+import { campaignDumData } from "../../atoms/atoms"
 
 export const Container1300Ver2 = styled(Container1300)`
   padding: 170px 0;
@@ -24,12 +24,17 @@ const CreateCampaignBtn = styled(ConfirmButton)`
   width:auto;
 `
 
+const CampaignList = styled.div`
 
+`
 
 
 export default function CampaignPage() {
-  const [values, setValues] = useState(['모집 중', '진행예정', '모집마감']);
+  const dum = useRecoilValue(campaignDumData);
+  const [values, setValues] = useState(['모집 중', '모집예정', '모집마감']); 
   const [currentValue, setCurrentValue] = useState('모집 중');
+  const filteredStatus = dum.filter(ele => ele.status === currentValue);
+  
   return (
     <Container>
       <Container1300Ver2>
@@ -39,7 +44,11 @@ export default function CampaignPage() {
             <CreateCampaignBtn>새로운 캠페인 등록</CreateCampaignBtn>
           </Link>
         </AdditionalBox>
-        <CampaignItem />
+        <CampaignList >
+          {filteredStatus.map(props =>(
+            <CampaignItem {...props} />
+          ))}
+        </CampaignList>
       </Container1300Ver2>
     </Container>
   );
