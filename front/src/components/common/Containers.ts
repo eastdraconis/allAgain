c0b0af5d9e5fb89eb35291fb122168d53592d3fe
KimKW1007 <face676934@gmail.com>
2022-10-14 17:58:50 +0900

    style : container padding 추가

--- front/src/components/common/Containers.ts
+++ front/src/components/common/Containers.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100%;
   max-width: 1920px;
-  padding: 0 80px;
+  padding: 170px 80px 100px;
   margin: 0 auto;
 `;
 
