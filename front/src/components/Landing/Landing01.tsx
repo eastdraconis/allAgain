ebd9bfae976fa563ec049412dc3f188f512a1c46
KimKW1007 <face676934@gmail.com>
2022-10-16 23:25:15 +0900

    chore & test & style : innerbox 추가 및 style 변경

--- front/src/components/Landing/Landing01.tsx
+++ front/src/components/Landing/Landing01.tsx
@@ -9,19 +9,25 @@ const Section = styled.section`
   height: calc(100vh - 70px);
   text-align:center;
   background: #d0e2db;
+  overflow: hidden;
+  &.active{
+    > div{
+      transform: scale(1);
+      .titleBox, .mainDescTextBox{
+        opacity: .5;
+      }
+    }
+  }
+`
+
+const InnerBox = styled.div`
+  position: relative;
+  height:100%;
   transform-origin:50% 0 ;
   transition : transform 2s;
   transform: scale(11);
-  overflow: hidden;
-  &.active{
-    transform: scale(1);
-    .titleBox, .mainDescTextBox{
-      opacity: .5;
-    }
-
-  }
-  
 `
+
 const TitleBox = styled.div`
   opacity: 0;
   position: relative;
@@ -77,16 +83,18 @@ export default function Landing01() {
   
   return (
     <Section id="section01" className={isLoading ? 'active' : ""}>
-      <TitleBox className="titleBox">
-        <img src={LandingImgs.landingTitle} alt="다시, 다 프로젝트" />
-      </TitleBox>
-      <MainDescTextBox className="mainDescTextBox">
-        <p>당 신 은 지 구 를 위해</p>
-        어떤 노력을 하고 있나요?
-      </MainDescTextBox>
-      {LandingImgs.section01Imgs.map((ele,idx) => (
-        <MountainsParallaxItems isLoading={isLoading} bgImage={ele} idx={idx} zIndex={zIndex[idx]}/>
-      ))}
+      <InnerBox>
+        <TitleBox className="titleBox">
+          <img src={LandingImgs.landingTitle} alt="다시, 다 프로젝트" />
+        </TitleBox>
+        <MainDescTextBox className="mainDescTextBox">
+          <p>당 신 은 지 구 를 위해</p>
+          어떤 노력을 하고 있나요?
+        </MainDescTextBox>
+        {LandingImgs.section01Imgs.map((ele,idx) => (
+          <MountainsParallaxItems isLoading={isLoading} bgImage={ele} idx={idx} zIndex={zIndex[idx]}/>
+        ))}
+      </InnerBox>
     </Section>
   )
 }
