8b294182c15f4e4fa4d5cb6df0cae90ccd3991b8
KimKW1007 <face676934@gmail.com>
2022-10-17 16:32:08 +0900

    chore & style : map key값 부여 , landing02 style 부여

--- front/src/components/Landing/Landing02.tsx
+++ front/src/components/Landing/Landing02.tsx
@@ -1,14 +1,17 @@
 import styled from "styled-components"
+import IsKnow from "./IsKnow"
 
 
 const Section = styled.section`
 
 `
 
+
+
 export default function Landing02() {
   return (
     <Section id="section02">
-      
+      <IsKnow/>
     </Section>
   )
 }
