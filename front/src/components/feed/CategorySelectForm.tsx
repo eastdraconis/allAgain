4b1ed12298678a8b86a202f9642bdf4e0bd500b0
DaHyunOhe <peelsob4d@gmail.com>
2022-10-17 11:42:36 +0900

    chore: 안내문구 추가

--- front/src/components/feed/CategorySelectForm.tsx
+++ front/src/components/feed/CategorySelectForm.tsx
@@ -22,23 +22,31 @@ const CategoryList = {
 function CategorySelectForm({ register, category }: CategorySelectFormProps) {
   return (
     <CategoryFormContainer>
-      {Object.entries(CategoryList).map(([key, value]) => (
-        <CategoryCheckbox
-          register={register}
-          isSelected={category.split(",").includes(value)}
-          checkboxValue={value}
-          checkboxKey={key}
-          key={key}
-        />
-      ))}
+      카테고리를 선택해 주세요
+      <CategoryLabel>
+        {Object.entries(CategoryList).map(([key, value]) => (
+          <CategoryCheckbox
+            register={register}
+            isSelected={category.split(",").includes(value)}
+            checkboxValue={value}
+            checkboxKey={key}
+            key={key}
+          />
+        ))}
+      </CategoryLabel>
     </CategoryFormContainer>
   );
 }
 
 const CategoryFormContainer = styled.div`
+  color: ${({ theme }) => theme.colors.dasidaGreen};
   width: 1200px;
   padding: 14px 45px 14px 45px;
   background-color: white;
+`;
+const CategoryLabel = styled.div`
+  margin-top: 8px;
+  width: 100%;
   display: flex;
 `;
 
