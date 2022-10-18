a82384b225a0e2a278e4f5754e77515c0784c505
KimKW1007 <face676934@gmail.com>
2022-10-18 20:54:51 +0900

    refactor : like 버튼 button 으로 변경

--- front/src/components/common/LikeToggle.tsx
+++ front/src/components/common/LikeToggle.tsx
@@ -3,51 +3,32 @@ import { useState, ChangeEvent } from "react";
 import LikeIconOn from "../../assets/images/icons/icon_like_on.png"
 import LikeIconOff from "../../assets/images/icons/icon_like_off.png"
 
-const LikeLabel = styled.label`
+const LikeButton = styled.button`
   display: block;
   width: 30px;
   height: 30px;
   background: url(${LikeIconOff}) no-repeat 50% 50%/contain;
-  text-indent: -99999px;
-  font-size: 0;
   transition: all .3s;
-  cursor: pointer;
-
-  & + input[type="checkbox"] {
-    display: none;
-    visibility: hidden;
-  }
-
   &:hover {
     transform: scale(.9);
   }
-
   &.active {
     background-image: url(${LikeIconOn})
   }
-
-`;
-
+`
 
 export default function LikeToggle() {
 
   const [isActive, setIsActive] = useState(false);
 
-  function handleOnChange(e: ChangeEvent<HTMLInputElement>) {
-    setIsActive(!isActive);
+  const handleOnChange = () => {
+    setIsActive(prev => !prev);
   }
 
   return (
     <div>
-      <LikeLabel 
-        htmlFor="LikeToggle" 
-        className={isActive ? "active" : ""}>좋아요</LikeLabel>
-      <input 
-        id="LikeToggle"
-        type="checkbox" 
-        checked={isActive}
-        onChange={handleOnChange}
-      />
+      <LikeButton className={isActive ? "active" : ""} onClick={handleOnChange}></LikeButton>
+      
     </div>
   )
 }
