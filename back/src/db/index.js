ca9e89ec1046cdbc2dac43b346af4e2ac4618422
jaehee <jaeheelee169@gmail.com>
2022-10-18 17:33:52 +0900

    refactor: 환경변수 추가

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2";
 
 const pool = mysql.createPool({
-  host: "34.64.61.16",
-  user: "testuser",
-  password: "test",
-  database: "TESTDB",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   enableKeepAlive: true,
