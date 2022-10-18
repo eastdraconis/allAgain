467c4ff355639c4818a87a5b1832d0838df71adc
DaHyunOhe <peelsob4d@gmail.com>
2022-10-18 14:57:56 +0900

    feat: 관리자 피드 스타일 추가

--- front/src/components/common/FollowToggleSmall.tsx
+++ front/src/components/common/FollowToggleSmall.tsx
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 import { useState, ChangeEvent } from "react";
-import CheckIconGreen from "../../assets/images/icons/icon_check_gr.png"
-import CheckIconWhite from "../../assets/images/icons/icon_check_wh.png"
+import CheckIconGreen from "../../assets/images/icons/icon_check_gr.png";
+import CheckIconWhite from "../../assets/images/icons/icon_check_wh.png";
 
-const FollowLabel = styled.label`
+const FollowLabel = styled.label<{ isAdmin: boolean }>`
   background: ${({ theme }) => theme.colors.dasidaGreen};
   color: ${({ theme }) => theme.colors.white};
-  border: 1px solid ${({ theme }) => theme.colors.dasidaGreen};
+  border: 1px solid
+    ${({ isAdmin, theme }) =>
+      isAdmin ? theme.colors.white : theme.colors.dasidaGreen};
 
   position: relative;
   display: block;
@@ -29,26 +31,29 @@ const FollowLabel = styled.label`
     top: 50%;
     left: 5px;
     transform: translate(0, -50%);
-    width: .9em;
-    height: .9em;
-    background: url(${CheckIconGreen}) no-repeat 50% 50%/contain;
-    transition: all .3s;
+    width: 0.9em;
+    height: 0.9em;
+    background: url(${CheckIconGreen}) no-repeat 50% 50% / contain;
+    transition: all 0.3s;
   }
 
   &.active {
     color: ${({ theme }) => theme.colors.dasidaGreen};
-    background: transparent;
+    background: ${({ isAdmin, theme }) =>
+      isAdmin ? theme.colors.white : "transparent"};
     padding: 0 0 0 14px;
   }
 
   &.active:before {
     display: block;
   }
-
 `;
 
-export default function FollowToggleSmall() {
+interface FollowToggleSmallType {
+  isAdmin: boolean;
+}
 
+export default function FollowToggleSmall({ isAdmin }: FollowToggleSmallType) {
   const [isFollowed, setIsFollwed] = useState(false);
 
   function handleOnChange(e: ChangeEvent<HTMLInputElement>) {
@@ -57,15 +62,22 @@ export default function FollowToggleSmall() {
 
   return (
     <div>
-      <FollowLabel 
-        htmlFor="followToggleSmall" 
-        className={isFollowed ? "active" : ""}>팔로우</FollowLabel>
-      <input 
+      <FollowLabel
+        isAdmin={isAdmin}
+        htmlFor="followToggleSmall"
+        className={isFollowed ? "active" : ""}>
+        팔로우
+      </FollowLabel>
+      <input
         id="followToggleSmall"
-        type="checkbox" 
+        type="checkbox"
         checked={isFollowed}
         onChange={handleOnChange}
       />
     </div>
-  )
+  );
 }
+
+FollowToggleSmall.defaultProps = {
+  isAdmin: false,
+};
