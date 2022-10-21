b33c43b8688370dd6e7d07d79dab6e5e26197bd5
DaHyunOhe <peelsob4d@gmail.com>
2022-10-21 21:26:07 +0900

    fix: 문구 수정 및 없는주소 리다이렉트 임시 수정

--- front/src/pages/UserPage/UserDetailPage.tsx
+++ front/src/pages/UserPage/UserDetailPage.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { getUserProfileById } from "../../api/userApi";
 import { loggedInUserId } from "../../atoms/atoms";
@@ -9,12 +9,14 @@ import UserListSelectForm from "../../components/UserDetail/UserListSelectForm";
 function UserPage() {
   const { id } = useParams();
   const currentUserId = useRecoilValue(loggedInUserId);
+  const navigator = useNavigate();
 
   const userQuery = useQuery(
     ["userProfile", id],
     () => getUserProfileById(id!, currentUserId),
     {
       refetchOnMount: true,
+      onError: () => navigator(-1),
     }
   );
   return (
